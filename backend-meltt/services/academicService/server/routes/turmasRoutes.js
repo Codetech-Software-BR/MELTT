c433117da7f969cd63e12e5b24f6824a67ebfd57
Guilherme <guilhermekrauseramos@gmail.com>
2025-02-18 11:56:05 -0400

    feat: adicionando arquivos routes

---
+++ backend-meltt/services/academicService/server/routes/turmasRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import turmaController from "../controllers/turmaController.js";
+import authMiddleware from "../middlewares/auth/index.js";
+
+const router = express.Router();
+
+router.get("/", authMiddleware, turmaController.getAllTurmas);
+router.get("/:id", authMiddleware, turmaController.getTurmaById);
+router.get("/faculdade/:id", authMiddleware, turmaController.getTurmaByFaculdadeId);
+router.post("/", authMiddleware, turmaController.createTurma);
+router.patch("/:id", authMiddleware, turmaController.updateTurma);
+router.delete("/:id", authMiddleware, turmaController.deleteTurma);
+
+export default router;
