61ae6df82b6207e04260b55c06b02d46ffe07b34
unknown <lucasdesouzasilva112@gmail.com>
2025-02-25 14:38:02 -0300

    criação de usuário aluno para acesso no sistema e assinatura de associação

--- frontend-meltt/src/pages/pagamentos/index.tsx
+++ frontend-meltt/src/pages/pagamentos/index.tsx
@@ -12,21 +12,26 @@ import {
   Stack,
   TableCell,
   TableRow,
+  TextField,
   Tooltip,
 } from "@mui/material";
 import BasicTable from "../../components/table";
 import { Key, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { apiGetData } from "../../services/api";
+import { apiGetData, apiPostData } from "../../services/api";
 import { EnumStudentBasicEducation } from "../../utils/enums";
 import toast from "react-hot-toast";
 import NoTableData from "../../components/noData";
 import LoadingTable from "../../components/loadingTable";
 import { MdOutlinePayments } from "react-icons/md";
-import { FaEye } from "react-icons/fa6";
+import { FaEye, FaUserGraduate } from "react-icons/fa6";
 import { pagamentosColumns } from "./table/columns";
 import { format } from "date-fns";
 import { BiSearch } from "react-icons/bi";
+import { getToken } from "../../utils/token";
+import { jwtDecode } from "jwt-decode";
+import { CustomJwtPayload } from "../../components/customDrawer";
+import CustomModal from "../../components/modal";
 
 interface Student {
   id: number;
@@ -47,10 +52,17 @@ interface Student {
 
 const PagamentosPage = () => {
   const navigate = useNavigate();
+  const token = getToken();
+  const decoded = token ? jwtDecode<CustomJwtPayload>(token) : null;
+
+  const [openModalCreateUser, setOpenModalCreateUser] = useState(false);
   const [loading, setLoading] = useState(false);
   const [payments, setPayments] = useState([]);
+  const [payment, setPayment] = useState<Student | null>(null);
 
   const [onLoad, setOnLoad] = useState(false);
+  const [loadingSaveNewUser, setLoadingSaveNewUser] = useState(false);
+
   const [page, setPage] = useState(1);
   const [filterSituation, setFilterSituation] = useState<string | null>(null);
   const [filterDate, setFilterDate] = useState<string | null>(null);
@@ -59,7 +71,7 @@ const PagamentosPage = () => {
     setLoading(true);
     try {
       const params = new URLSearchParams();
-      
+
       params.append("pagina", page.toString());
       if (filterSituation) {
         params.append("situacoes", filterSituation);
@@ -67,7 +79,7 @@ const PagamentosPage = () => {
       if (filterDate) {
         params.append("dataInicial", filterDate);
       }
-  
+
       const response = await apiGetData("academic", `/bling/contas/receber?${params.toString()}`);
       setPayments(response.data);
     } catch (error) {
@@ -75,18 +87,17 @@ const PagamentosPage = () => {
     }
     setLoading(false);
   };
-  
+
   const fetchWithFilters = async () => {
     setLoading(true);
-
     try {
       const params = new URLSearchParams();
-  
+
       if (filterSituation) {
         params.append("situacoes", filterSituation);
       }
       if (filterDate) params.append("dataInicial", filterDate);
-  
+
       const response = await apiGetData("academic", `/bling/contas/receber?${params.toString()}`);
       setPayments(response.data);
     } catch (error) {
@@ -96,8 +107,7 @@ const PagamentosPage = () => {
 
   }
 
-
-  const handleChangePagination = (event: React.ChangeEvent<unknown>, value: number) => {
+  const handleChangePagination = (_: React.ChangeEvent<unknown>, value: number) => {
     try {
       fetchPagamentos(value);
     } catch (error) {
@@ -105,6 +115,33 @@ const PagamentosPage = () => {
     }
     setPage(value);
   };
+
+  const onSubmitNewUser = async (values: Student) => {
+    setLoadingSaveNewUser(true);
+
+    let dataObj = {
+      nome: payment?.contato.nome,
+      documento: payment?.contato.numeroDocumento,
+      email: `${payment?.contato.numeroDocumento}@meltt.com.br`,
+      senha: payment?.id.toString(),
+      id_bling: payment?.id.toString(),
+      tipo: "ALUNO",
+    };
+
+    try {
+      const response = await apiPostData("authentication", "/users/register", { ...dataObj });
+      if (response.user) {
+        toast.success(`Usuário criado com sucesso. E-mail: ${payment?.contato.numeroDocumento}@meltt.com.br  | Senha: ${payment?.id}`, {
+          duration: 20000,
+          icon: "👏",
+        });
+        setOpenModalCreateUser(false);
+      }
+    } catch (error) {
+      toast.error("Erro ao criar usuário para aluno");
+    }
+    setLoadingSaveNewUser(false);
+  }
 
   const dataRow = (row: Student) => {
     return (
@@ -143,11 +180,25 @@ const PagamentosPage = () => {
           />
         </TableCell>
         <TableCell align="left" sx={{ fontFamily: "Poppins" }}>
-          <Tooltip title="Visualizar Boleto">
-            <IconButton size="small" onClick={() => window.location.href = row.linkBoleto}>
-              <FaEye color="#2d1c63" size={22} />
-            </IconButton>
-          </Tooltip>
+          <Stack direction={'row'} alignItems={'center'} gap={2}>
+            <Tooltip title="Visualizar Boleto">
+              <IconButton size="small" onClick={() => window.location.href = row.linkBoleto}>
+                <FaEye color="#2d1c63" size={22} />
+              </IconButton>
+            </Tooltip>
+            {decoded?.tipo === 'ADMIN' && (
+              <Tooltip title="Criar Usuário para Aluno">
+                <IconButton
+                  size="small"
+                  onClick={() => {
+                    setPayment(row)
+                    setOpenModalCreateUser(true)
+                  }}>
+                  <FaUserGraduate size={20} />
+                </IconButton>
+              </Tooltip>
+            )}
+          </Stack>
         </TableCell>
       </TableRow>
     );
@@ -237,6 +288,57 @@ const PagamentosPage = () => {
           </Paper>
         </Paper>
       </Slide>
+      <CustomModal
+        title="Criar usuário para Aluno"
+        subHeader="o documento e a senha usada aqui será para o aluno acessar o sistema"
+        openModal={openModalCreateUser}
+        onSubmit={onSubmitNewUser}
+        loadingSave={loadingSaveNewUser}
+        handleCloseModal={() => setOpenModalCreateUser(false)}
+      >
+        <Stack direction={"column"} gap={2}>
+          <TextField
+            label="Nome Completo"
+            variant="outlined"
+            focused
+            fullWidth
+            value={payment?.contato.nome}
+            size="small"
+          />
+          <TextField
+            label="Documento(CPF)"
+            variant="outlined"
+            fullWidth
+            size="small"
+            value={payment?.contato.numeroDocumento}
+            disabled
+          />
+          <TextField
+            label="Senha do Aluno"
+            variant="outlined"
+            fullWidth
+            size="small"
+            value={payment?.id}
+            disabled
+          />
+          <TextField
+            label="E-mail para acesso do usuário (ANOTE PARA NÃO PERDER)"
+            variant="outlined"
+            fullWidth
+            size="small"
+            value={`${payment?.contato.numeroDocumento}@meltt.com.br`}
+            disabled
+          />
+          <TextField
+            label="Identificador Bling"
+            variant="outlined"
+            fullWidth
+            size="small"
+            value={payment?.id}
+            disabled
+          />
+        </Stack>
+      </CustomModal>
     </Stack>
   );
 };
