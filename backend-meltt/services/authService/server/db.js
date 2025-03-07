af96a633eae7450623d0b50cc5adb0ad7fd23df1
unknown <lucasdesouzasilva112@gmail.com>
2025-03-07 09:27:26 -0300

    tentativa de ajuste de timeout no railway

--- backend-meltt/services/authService/server/db.js
+++ backend-meltt/services/authService/server/db.js
@@ -10,7 +10,10 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  keepAliveInitialDelay:0,
+  enableKeepAlive: true,
 });
 
 
