ca813b6011d702184295578203dcaee872c98a69
unknown <lucasdesouzasilva112@gmail.com>
2025-05-10 01:18:01 -0300

    trazendo d4sign para backend

---
+++ backend-meltt/services/academicService/server/routes/d4SignRoutes.js
@@ -0,0 +1,64 @@
+import express from "express";
+import axios from 'axios';
+import authMiddleware from "../middlewares/auth/index.js";
+
+const router = express.Router();
+
+router.get("/estatuto", authMiddleware, async (req, res) => {
+  try {
+    const file = req.files.file;
+
+    const formData = new FormData();
+    formData.append('file', file.data, { filename: file.name });
+
+    const response = await axios.post(
+      'https://secure.d4sign.com.br/api/v1/documents/upload',
+      formData,
+      {
+        params: {
+          tokenAPI: process.env.D4SIGN_TOKEN_API,
+          cryptKey: process.env.D4SIGN_CRYPTKEY,
+          uuidSafe: process.env.D4SIGN_ESTATUTOS_SAFER
+        },
+        headers: {
+          ...formData.getHeaders()
+        }
+      }
+    );
+
+    res.json({ uuid: response.data.uuid });
+  } catch (error) {
+    res.status(500).json({ error: "Falha no upload" });
+  }
+});
+
+router.get("/contrato-meltt", authMiddleware, async (req, res) => {
+  try {
+    const file = req.files.file;
+
+    const formData = new FormData();
+    formData.append('file', file.data, { filename: file.name });
+
+    const response = await axios.post(
+      'https://secure.d4sign.com.br/api/v1/documents/upload',
+      formData,
+      {
+        params: {
+          tokenAPI: process.env.D4SIGN_TOKEN_API,
+          cryptKey: process.env.D4SIGN_CRYPTKEY,
+          uuidSafe: process.env.D4SIGN_CONTRATO_MELTT_SAFER
+        },
+        headers: {
+          ...formData.getHeaders()
+        }
+      }
+    );
+
+    res.json({ uuid: response.data.uuid });
+  } catch (error) {
+    res.status(500).json({ error: "Falha no upload" });
+  }
+});
+
+
+export default router;
